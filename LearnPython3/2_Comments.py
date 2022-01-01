86de82f920bb53c824438b5ac5b21ae7944446ef
unknown <danielzarate317@gmail.com>
2022-01-01 16:35:23 -0500

    first commit

---
+++ LearnPython3/2_Comments.py
@@ -0,0 +1,17 @@
+#Ironically, the first thing we’re going to do is show how to tell a computer to ignore a part of a program. 
+# Text written in a program but not run by the computer is called a comment. Python interprets anything after a # as a comment.
+
+#Comments can:
+
+#Provide context for why something is written the way it is:
+
+# This variable will be used to count the number of times anyone tweets the word persnickety
+persnickety_count = 0
+
+#Help other people reading the code understand it faster:
+
+# This code will calculate the likelihood that it will rain tomorrow
+#complicated_rain_calculation_for_tomorrow()
+#Ignore a line of code and see how a program will run without it:
+
+#I'm doing a program on how to always find how much does a gram of gold costs depending on it's kt
