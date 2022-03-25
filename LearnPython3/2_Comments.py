57bcc023988545357ab77775b3805b624373c959
Daniel <danielzarate317@gmail.com>
2022-03-24 20:08:31 -0500

    Hello

--- LearnPython3/2_Comments.py
+++ LearnPython3/2_Comments.py
@@ -15,3 +15,6 @@ persnickety_count = 0
 #Ignore a line of code and see how a program will run without it:
 
 #I'm doing a program on how to always find how much does a gram of gold costs depending on it's kt
+lista_x = [1,2,3,4]
+sorted_lista_X = lista_x.sort()
+print(sorted_lista_X)
